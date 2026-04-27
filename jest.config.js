9db12c33ad6574d3a707c83a5d3817f60cd05057
Jase Thomas <jase.thomas04@gmail.com>
2026-04-27 16:15:04 -0400

    Configure automated jest tests with jsdom environment

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom",
+
+  testMatch: [
+    "<rootDir>/tests/unit/**/*.test.js",
+    "<rootDir>/tests/integration/**/*.test.js"
+  ],
+
+  verbose: true
+};
